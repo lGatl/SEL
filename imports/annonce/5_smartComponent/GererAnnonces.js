fe0b358643174e9f47f1b8d7c6afc45ee8d3444f
lGatl <gat55@live.fr>
2018-05-16 16:28:39 +0200

    scroll infini gerer annonces

--- imports/annonce/5_smartComponent/GererAnnonces.js
+++ imports/annonce/5_smartComponent/GererAnnonces.js
@@ -7,25 +7,43 @@ import { ACTIONS } from "../../6_actions/actions";
 
 import { dateToFormat } from "../../8_libs/date";
 import { hrefUser, hrefAnnonce } from "../../8_libs/go";
+import { throttle } from "../../8_libs/throttle";
 
 import { Input, TextArea, Button, Tableau, Dropdown, Titre, A } from "../../_common/4_dumbComponent/_gat_ui_react";
 
 
 class GererAnnonces extends Component {
-
+	constructor(){
+		super();
+		this.scroll = throttle(this.scroll.bind(this),40);
+		this.state = {
+			nbpp: 10,
+			nump: 0
+		};
+	}
 	componentWillMount(){
 		this.props.titrePage("Gerer les annonces");
 		this.props.activeMenu("Mon Compte");
 		this.props.activeMenuMonCompte("Gerer les annonces");
-		this.props.annonceGetSSL({},{sort:{date:-1}},res=>{
-			this.props.usersGet({_id:{$in:res.map(annonce=>annonce.user_id)}});
-			this.props.categorieGet({_id:{$in:res.map(annonce=>annonce.categorie)}});
-			res.map(annonce=>{
+		this.props.categorieGet({});
+		this.props.annonceGetSSL({},{sort:{date:-1},skip:0,limit:this.state.nbpp},annonces=>{
+			this.setState({nump:1});
+			this.props.usersGet({_id:{$in:annonces.map(annonce=>annonce.user_id)}},()=>{
+				this.props.annonceCount({},(nb_annonces)=>{
+					this.scroll(annonces,nb_annonces);
+				});
 			});
-			this.props.annonceControle({actions:res.map((annonce)=>{return{_id:annonce._id,action:annonce.etat};})});
+			
+			this.props.annonceControle({actions:annonces.map((annonce)=>{return{_id:annonce._id,action:annonce.etat};})});
 		});
 	}
-	
+	componentDidMount() {
+		document.addEventListener("scroll", this.scroll);
+	}
+
+	componentWillUnmount() {
+		document.removeEventListener("scroll", this.scroll);
+	}
 	//==============CONTROLE====================
 	changeAction(e,{ value, name }){
 		let Actions = this.props.annonce_controle.actions;
@@ -37,7 +55,24 @@ class GererAnnonces extends Component {
 		this.props.annonceControle({actions:[...actions]});
 	}
 	//==============ACTION====================
-
+	scroll(annonces,nb_annonces){
+		if(
+			((window.scrollY >= (document.documentElement.scrollHeight - document.documentElement.clientHeight)*0.95)||
+			(document.documentElement.scrollHeight - document.documentElement.clientHeight)==0)
+			&& ((this.props.annonces.length < this.props.nb_annonces)||(annonces&&nb_annonces&&annonces.length < nb_annonces))
+		){
+			this.props.annonceGetAddSSL({},{sort:{date:-1},skip:((this.state.nump)*this.state.nbpp),limit:this.state.nbpp},(nv_annonces)=>{
+				this.props.usersGetAdd({_id:{$in:nv_annonces.map(annonce=>annonce.user_id)}},()=>{
+					this.props.annonceCount({},(nb_annonces)=>{
+						this.scroll(annonces,nb_annonces);
+					});
+					this.props.annonceControle({actions:[...this.props.annonce_controle.actions, ...nv_annonces.map((annonce)=>{return{_id:annonce._id,action:annonce.etat};})]});
+				});
+				
+			});
+			this.setState({nump:this.state.nump+1});
+		}
+	}
 	annonceAdd(){
 		let {titre} = this.props.annonce_controle;
 
@@ -83,6 +118,7 @@ class GererAnnonces extends Component {
 		
 		return (
 			<div>
+				<Button onClick={this.annonceAppliquer.bind(this)}>Appliquer</Button>
 				<Tableau
 					ligne1sur2
 					border_line
@@ -122,9 +158,7 @@ class GererAnnonces extends Component {
 									}}
 								/>];}):[]},
 					]}
-				/>
-				<Button onClick={this.annonceAppliquer.bind(this)}>Appliquer</Button>
-				
+				/>		
 			</div>
 		);
 	}
@@ -135,6 +169,7 @@ function mapStateToProps( state ){
 		{
 			annonce_controle: state.annonce.controle,
 			annonces: state.annonce.all,
+			nb_annonces: state.annonce.count,
 			users: state.users.all,
 			categories: state.categorie.all,
 		}
@@ -147,12 +182,16 @@ function mapDispatchToProps( dispatch ){
 		activeMenu: ACTIONS.Menu.activeMenu,
 		activeMenuMonCompte: ACTIONS.Menu.activeMenuMonCompte,
 		annonceGetSSL: ACTIONS.Annonce.get_SSL,
+		annonceGetAddSSL: ACTIONS.Annonce.getAdd_SSL,
+		annonceCount: ACTIONS.Annonce.count,
+
 		annonceControle: 	ACTIONS.Annonce.controle,
 		annonceAdd:	ACTIONS.Annonce.add,
 		annonceRm: 	ACTIONS.Annonce.rm,
 		annonceUp: 	ACTIONS.Annonce.up,
 
 		usersGet: 	ACTIONS.Users.get,
+		usersGetAdd: 	ACTIONS.Users.getAdd,
 		categorieGet: ACTIONS.Categorie.get,
 
 	}, dispatch );
